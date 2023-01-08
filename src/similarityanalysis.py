65cb523a191dc12298eee094a833d9bdca3cc05b
ehrenb <branden.a.ehrenreich@gmail.com>
2023-01-08 15:53:22 -0500

    convert to support neomodel, id->uid

--- src/similarityanalysis.py
+++ src/similarityanalysis.py
@@ -3,7 +3,6 @@ import json
 import ssdeep
 
 from machina.core.worker import Worker
-from machina.core.models.relationships.similar import Similar
 
 class SimilarityAnalysis(Worker):
 
@@ -20,8 +19,9 @@ class SimilarityAnalysis(Worker):
         comparison_rules = self.config['worker']['comparison_type_rules']
 
         # resolve obj
-        obj = self.graph.get_vertex(data['id'])
-        obj_node_type = obj.__class__.__name__.lower()
+        obj_cls = self.resolve_db_node_cls(data['type'])
+        obj_node_type = obj_cls.__name__.lower()
+        obj = obj_cls.nodes.get(uid=data['uid'])
 
         # TODO: smooth this out
         types_to_compare = []
@@ -29,7 +29,7 @@ class SimilarityAnalysis(Worker):
             # ['*']:['*'] everything is compared to everything, also *:['apk'] will act identically - "everything is compared to apk, and apk is compared to everything"
             if stype == '*' or '*' in ttypes:
                 types_to_compare = self.config['types']['available_types'].copy()
-                types_to_compare.remove('*')
+                # types_to_compare.remove('*')
                 break
             # ['apk']:['jar','dex']
             if obj_node_type == stype:
@@ -42,10 +42,10 @@ class SimilarityAnalysis(Worker):
 
         for type_to_compare in types_to_compare:
             c = self.resolve_db_node_cls(type_to_compare)
-            targets = c.objects.query()
+            targets = c.nodes.all()
             for t in targets:
                 # avoid comparing with self
-                if t._id == obj._id:
+                if t.uid == obj.uid:
                     continue
                 # ensure node to compare against has ssdeep computed
                 if t.ssdeep:
@@ -53,13 +53,16 @@ class SimilarityAnalysis(Worker):
                     result = ssdeep.compare(obj.ssdeep, t.ssdeep)
                     # check threshold
                     if result > ssdeep_threshold:
-                        self.logger.info(f"Establishing similarity link between {obj._id} {t._id} with result {result}")
+                        self.logger.info(f"Establishing similarity link between {obj.uid} {t.uid} with result {result}")
 
-                        # TODO: figure out how to add data to relationship
                         data = {
                             "measurements": {
                                 "ssdeep_similarity": result
                             }
                         }
-                        self.create_edge(Similar, obj._id, t._id, data=data)
 
+                        similarity_rel = obj.similar.connect(t, data).save()
+                    else:
+                        self.logger.info(f"ssdeep comparsion only resulted in similarity of: {result}, not enough for link")
+                else:
+                    self.logger.info(f"No ssdeep for {t.uid}")
